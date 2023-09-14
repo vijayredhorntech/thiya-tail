90e7d34ca00394eaa861ac035f556567cf31996d
vin33t <vineet.chd09@gmail.com>
2023-09-14 12:56:45 +0530

    Product Category

---
+++ app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => ['required'],
+            'image' => ['required'],
+            'description' => ['nullable'],
+        ];
+    }
+}
